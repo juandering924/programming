af4eeda65b85092a909bf18a83880f35bfe54340
juandering924 <58196054+juandering924@users.noreply.github.com>
2019-12-19 00:16:57 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+//The setup function only happens once
+function setup() {
+	createCanvas(500, 500); //create a 500px X 500px canvas
+}
+
+//The draw function happens over and over again
+function draw() {
+  background(173,216,230); //an RGB color for the canvas' background
+  //circle
+  stroke(255,100,127) // an RGB color for the circle's border
+  fill(234,745,127,235); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  ellipse(width/2,height/2,200,200)
+  triangle(300,202,150,300,43,22)
+ fill(0, 152, 253, 51);
+
+}
+function mousePressed() {
+  if (value === 0) {
+    value = 255;
+  } else {
+    value = 0;
+  
+textSize(32)
+text('Got.EEE', 150, 120);
+
+
+
+
+
+  ; // center of canvas, 20px dia
+
