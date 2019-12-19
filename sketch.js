f63ab78500c6051bf612706c3e4d6149d563f135
juandering924 <58196054+juandering924@users.noreply.github.com>
2019-12-19 00:23:30 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,15 +13,15 @@ function draw() {
   triangle(300,202,150,300,43,22)
  fill(0, 152, 253, 51);
 
-}
-function mousePressed() {
+
+  
+textSize(32)
+text('Got.EEE', 150, 120);}
+	function mousePressed() {
   if (value === 0) {
     value = 255;
   } else {
     value = 0;
-  
-textSize(32)
-text('Got.EEE', 150, 120);
 
 
 
